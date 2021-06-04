18dbe75d1e6d3d0b77eeb6b47e2d81caaed804c1
Alfahmi <gst.alfahmi@gmail.com>
2021-06-04 19:44:26 +0700

    Create Status.cs

---
+++ API/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Status
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
