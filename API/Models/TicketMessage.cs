1c1bf476964eeccfa043f9568d6665535ca97727
Alfahmi <gst.alfahmi@gmail.com>
2021-06-04 19:57:05 +0700

    Create TicketMessage.cs

---
+++ API/Models/TicketMessage.cs
@@ -0,0 +1,21 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    [Table("TB_T_TicketMessage")]
+    public class TicketMessage
+    {
+        public int ID { get; set; }
+        public Ticket Ticket { get; set; }
+        public Message Messages { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public TicketMessage()
+        {
+            this.CreatedDate = DateTime.Now;
+        }
+    }
+}
