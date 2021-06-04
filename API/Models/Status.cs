102edf895bf22f6cd0d1a0b7843501ca35e31c62
Alfahmi <gst.alfahmi@gmail.com>
2021-06-04 19:45:31 +0700

    Update Status.cs

--- API/Models/Status.cs
+++ API/Models/Status.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Models
 {
+    [Table("TB_M_Status")]
     public class Status
     {
         public int ID { get; set; }
