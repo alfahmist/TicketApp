58620c8e2396d4e85dc2f29a6b952e45bffdac24
Alfahmi <gst.alfahmi@gmail.com>
2021-06-04 20:09:34 +0700

    Update MyContext.cs : DbSet

--- API/Context/MyContext.cs
+++ API/Context/MyContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,16 @@ namespace API.Context
     {
         public MyContext(DbContextOptions<MyContext> options) : base(options)
         { }
-    }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketMessage> TicketMessages { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
 }
