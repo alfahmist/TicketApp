075c458c85b4794f032d9ad4e84e3f5724b44441
Alfahmi <gst.alfahmi@gmail.com>
2021-06-04 19:53:41 +0700

    Create Employee.cs

---
+++ API/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Employee
+    {
+        public int  ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+        public ICollection<Message> Messages{ get; set; }
+    }
+}
