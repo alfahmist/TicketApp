20b5074c13cbb6d8f66b1fdc865edab70f74c60e
Fahmi <gst.alfahmi@gmail.com>
2021-06-14 08:24:31 +0700

    Create StatusHistory.cs

---
+++ API/Models/StatusHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    [Table("TB_T_StatusHistory")]
+    public class StatusHistory
+    {
+        public Ticket TicketID { get; set; }
+        public Status StatusID { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
