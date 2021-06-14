963cb66b12ad353b819e119e79950371f095eb33
Fahmi <gst.alfahmi@gmail.com>
2021-06-14 09:43:32 +0700

    Update StatusHistory.cs

--- API/Models/StatusHistory.cs
+++ API/Models/StatusHistory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace API.Models
     [Table("TB_T_StatusHistory")]
     public class StatusHistory
     {
+    
+        public int ID { get; set; }
         public Ticket Ticket { get; set; }
         public Status Status { get; set; }
         public DateTime CreatedDate { get; set; }
