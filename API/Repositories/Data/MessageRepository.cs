c0553f76b0a9a13b1aac0a5b4cd2164d904a3a9c
Fahmi <gst.alfahmi@gmail.com>
2021-06-15 08:54:12 +0700

    Create MessageRepository.cs

---
+++ API/Repositories/Data/MessageRepository.cs
@@ -0,0 +1,19 @@
+﻿using API.Context;
+using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Repositories.Datas
+{
+    public class MessageRepository : GeneralRepository<Message, MyContext, int>
+    {
+        private readonly MyContext myContext;
+
+        public MessageRepository(MyContext myContext) : base(myContext)
+        {
+
+        }
+    }
+}
