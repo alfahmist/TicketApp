dea0d376a755d838c19a662726b2670125c5660a
Alfahmist <48145002+alfahmist@users.noreply.github.com>
2021-06-15 08:31:36 +0700

    Revert "Modul repository"

--- API/Repositories/Datas/EmployeeRepository.cs
+++ API/Repositories/Datas/EmployeeRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace API.Repositories.Datas
 {
-    public class EmployeeRepository : GeneralRepository<Employee, MyContext, int>
+    public class EmployeeRepository : GeneralRepository<Employee, MyContext, string>
     {
         private readonly MyContext myContext;
 
--- API/Startup.cs
+++ API/Startup.cs
@@ -36,8 +36,6 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseAuthentication();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
